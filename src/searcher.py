f9a5501e3584f14739c2f7dff212f731afaf7f88
Niels Joubert <njoubert@gmail.com>
2013-03-16 06:35:21 -0700

    woohoo more data

--- src/searcher.py
+++ src/searcher.py
@@ -1,6 +1,6 @@
 import cherrypy
 
-import dataCollector
+import main
 
 
 class form_response_page:
@@ -8,16 +8,16 @@ class form_response_page:
     @cherrypy.expose
     def index(self, *args, **kwargs):
         papertitle = kwargs['paper_name']
-        papersDict = dataCollector.getAllPapers(papertitle)
+        papersDict = main.getAllPapers(papertitle)
 
         papersSorted = sorted(papersDict.iteritems(), key=lambda x: x[1]['num_citations'], reverse=True)
 
         html = "<html><head></head><body>"
         html = html + "<h1>Citation Search Results</h1>"
-        html = html + "<h3>" + str(len(papersSorted)) + " cited (directly or indirectly) \"" + papertitle + "\"</h3>"
+        html = html + "<h3>" + str(len(papersSorted)) + " papers came after " + papertitle + "</h3>"
         html = html + "<table>"
         for t,a in papersSorted:
-            html = html + "<tr><td><a href='" + a['url_citations'] + "'> "+ str(a['num_citations']) + " Citations</a></td><td>" + a['year'] + "</td><td><a href='"+a['url']+"'>" + t + "</a></td></tr>"
+            html = html + "<tr><td>"+ str(a['num_citations']) + " Citations</td><td><a href='"+a['url']+"'>" + t + "</a></td></tr>"
         html = html + "</table></body></html>"
         return html
 
