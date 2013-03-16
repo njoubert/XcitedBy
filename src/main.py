9e05462a37fab0690b3675a60dc54e9861cae22c
Niels Joubert <njoubert@gmail.com>
2013-03-16 05:29:37 -0700

    uggh

--- src/main.py
+++ src/main.py
@@ -15,15 +15,36 @@ def main():
     print "  ", papers[0]['title'], papers[0]['papernumber']
     print "  ", "with", papers[0]['num_citations'], "citations"
 
+    # allStuff = scholar.citations_by_papernr(papernr);
+    # print "I downloaded", len(allStuff), "papers"
+    # for al in allStuff:
+    # 	print al.as_txt();
 
-    paperNumber = papers[0]['papernumber']
 
-    while (True):
-    	newCitations = scholar.citations_by_papernr(paperNumber)
+    allCitations = [];
+    toCheckCitations = [papers[0]];
+    while (len(toCheckCitations) > 0):
+    	paper = toCheckCitations.pop(0)
+    	if (not paper['papernumber']):
+    		print 'Missing a papernumber on', paper['title']
+    		continue
+    	newCitations = scholar.citations_by_papernr(paper['papernumber'])
+    	for art in newCitations:
+    		already = False
+    		for knownArt in allCitations:
+    			if (art['papernumber'] is knownArt['papernumber']):
+    				already = True
+    		if (not already):
+    			allCitations.append(art)
+    			#toCheckCitations.append(art)
+
+
 
 
     
-    print "Found citations for first paper:", len(citations)
+    print "Found citations for first paper:", len(allCitations)
+    for c in allCitations:
+    	print c.as_txt();
 
 
 if __name__ == "__main__":
